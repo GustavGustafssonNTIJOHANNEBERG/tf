df00acd60646c189d22c6eb60c031604fa415504
GustavGustafssonNTIJOHANNEBERG <gustav.gustafsson@elev.ga.ntig.se>
2024-03-06 15:12:16 +0100

    vecka 10

--- app.rb
+++ app.rb
@@ -5,3 +5,19 @@ require 'sqlite3'
 get('/')  do
     slim(:start)
 end 
+
+get('/login') do
+    
+end
+
+get('/write') do
+    slim(:write)
+end
+
+post('/write') do
+    title = params[:title]
+    text = params[:text]
+    db = SQLite3::Database.new("db/tabell.db")
+    db.execute("INSERT INTO post (text, ämne) VALUES (?,?)",title, artist_id)
+    redirect('/')
+end
